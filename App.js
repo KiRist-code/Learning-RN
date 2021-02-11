f2ce108eac0d7dd07b2e4a36b01da9a411df8621
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-02-11 18:09:37 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Loading from "./src/Loading";
+import * as Location from "expo-location";
+import { render } from 'react-dom';
+import { Alert } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yelloView}/>
-      <View style={styles.blueView}/>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  yelloView: {
-    flex:1,
-    backgroundColor: " yellow"
-  },
-  blueView:{
-    flex:3,
-    backgroundColor: "blue"
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { 
+        coords : { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
   }
-});
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
+}
